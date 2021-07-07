3baac56e8ea03ef1d7d2eaa7cb89aea550d572f5
Andre Botta <andre.botta@outcomex.com.au>
2021-07-07 20:24:38 +1000

    initial commit

---
+++ src/main/java/com/github/alcbotta/javaqquerydslmovies/JavaqquerydslmoviesApplication.java
@@ -0,0 +1,14 @@
+package com.github.alcbotta.javaqquerydslmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class JavaqquerydslmoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaqquerydslmoviesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/alcbotta/javaqquerydslmovies/JavaqquerydslmoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.alcbotta.javaqquerydslmovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaqquerydslmoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
